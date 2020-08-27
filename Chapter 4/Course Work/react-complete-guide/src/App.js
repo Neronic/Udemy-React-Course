02725a3ec605e2a7480b38b4f37d0a8699b786c8
Jacob <n3r0n1c@hotmail.com>
2020-08-27 08:56:12 -0400

    Update App.js
    Finished Chapter 4, Now just the assignment

--- Chapter 4/Course Work/react-complete-guide/src/App.js
+++ Chapter 4/Course Work/react-complete-guide/src/App.js
@@ -13,14 +13,18 @@ class App extends Component {
     showPersons: false,
   };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: "Jacob", age: 26 },
-        { name: event.target.value, age: 26 },
-        { name: "Mo", age: 25 },
-      ],
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
     });
+
+    const person = { ...this.state.persons[personIndex] };
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
 
   deletePersonHandler = (personIndex) => {
@@ -56,6 +60,7 @@ class App extends Component {
                 name={person.name}
                 age={person.age}
                 key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
               />
             );
           })}
