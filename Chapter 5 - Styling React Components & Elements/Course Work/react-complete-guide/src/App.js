fe65557294e759b39a3312412d5867ab75074fe1
Jacob <n3r0n1c@hotmail.com>
2020-08-30 14:53:52 -0400

    Chapter 5 progress
    Added radium. Radium allows styling functionality.

---
+++ Chapter 5 - Styling React Components & Elements/Course Work/react-complete-guide/src/App.js
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
+import Radium from "radium";
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: "df4", name: "Jacob", age: 26 },
+      { id: "f4s", name: "Nicholas", age: 26 },
+      { id: "gd4", name: "Mohit", age: 25 },
+    ],
+    otherState: "some other value",
+    showPersons: false,
+  };
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
+
+    const person = { ...this.state.persons[personIndex] };
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  };
+
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  render() {
+    const style = {
+      backgroundColor: "green",
+      color: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black",
+      },
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+      style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "salmon",
+        color: "black",
+      };
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push("red"); //classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push("bold"); //classes = ['red', 'bold]
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(" ")}>This is really working!</p>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
+        {persons}
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
+}
+
+export default Radium(App);
