cae3abf48ede605d9d8c3c9e18483876b7d42335
Jacob <n3r0n1c@hotmail.com>
2020-09-01 12:36:15 -0400

    Chapter 7 progress

--- Chapter 7 - Diving Deeper Into Components & React Internals/Course Work/react-complete-guide/src/containers/App.js
+++ Chapter 7 - Diving Deeper Into Components & React Internals/Course Work/react-complete-guide/src/containers/App.js
@@ -24,12 +24,21 @@ class App extends Component {
     return state;
   }
 
-  componentWillMount() {
-    console.log("[App.js] componentWillMount");
-  }
+  //componentWillMount() {
+  //console.log("[App.js] componentWillMount");
+  //}
 
   componentDidMount() {
     console.log("[App.js] componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[App.js] shouldComponentUpdate");
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log("[App.js] componentDidUpdate");
   }
 
   nameChangedHandler = (event, id) => {
