ddf4b9a1d2d49157fcaacabdc6d3beb51161bc98
Jacob <n3r0n1c@hotmail.com>
2020-09-05 11:41:51 -0400

    Chapter 11 progress. Set up routing and learned how to use Links

--- Chapter 11 - Multi-Page-Feeling in a Single-Page-App - Routing/Course Work/http-work/src/containers/Blog/Blog.js
+++ Chapter 11 - Multi-Page-Feeling in a Single-Page-App - Routing/Course Work/http-work/src/containers/Blog/Blog.js
@@ -1,65 +1,38 @@
 import React, { Component } from "react";
 //import axios from "axios"
-import axios from "../../axios";
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
+import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
+import { Route, Link } from "react-router-dom";
+
 import "./Blog.css";
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false,
-  };
-
-  componentDidMount() {
-    axios
-      .get("/posts")
-      .then((response) => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map((post) => {
-          return {
-            ...post,
-            author: "Jacob",
-          };
-        });
-        this.setState({ posts: updatedPosts });
-        //console.log(response);
-      })
-      .catch((error) => {
-        this.setState({ error: true });
-      });
-  }
-
-  posSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
-  };
-
   render() {
-    let posts = <p style={{ textAlign: "center" }}>Something went wrong!</p>;
-    if (!this.state.error) {
-      posts = this.state.posts.map((post) => {
-        return (
-          <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.posSelectedHandler(post.id)}
-          />
-        );
-      });
-    }
-
     return (
-      <div>
-        <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link
+                  to={{
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true",
+                  }}
+                >
+                  New Post
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
