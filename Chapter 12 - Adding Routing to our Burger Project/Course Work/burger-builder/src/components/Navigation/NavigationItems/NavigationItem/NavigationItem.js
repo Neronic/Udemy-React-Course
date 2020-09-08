6f6cc48ccb5267c293bbe6d402156968e76931b8
Jacob <n3r0n1c@hotmail.com>
2020-09-08 12:53:24 -0400

    Chapter 12 complete

--- Chapter 12 - Adding Routing to our Burger Project/Course Work/burger-builder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ Chapter 12 - Adding Routing to our Burger Project/Course Work/burger-builder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./NavigationItem.css";
 
 const navigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <a href={props.link} className={props.active ? classes.active : null}>
+    <NavLink exact activeClassName={classes.active} to={props.link}>
       {props.children}
-    </a>
+    </NavLink>
   </li>
 );
 
--- Chapter 12 - Adding Routing to our Burger Project/Course Work/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Chapter 12 - Adding Routing to our Burger Project/Course Work/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,29 +88,21 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     //alert("You continue!");
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Jacob Fields",
-        address: {
-          street: "Teststreet 1",
-          zipCode: "4234234",
-          country: "Canada",
-        },
-        email: "test@test.com",
-      },
-      deliveryMethod: "fastest",
-    };
-    axios
-      .post("/orders.json", order)
-      .then((response) => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch((error) => {
-        this.setState({ loading: false, purchasing: false });
-      });
+
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          "=" +
+          encodeURIComponent(this.state.ingredients[i])
+      );
+    }
+    queryParams.push("price=" + this.state.totalPrice);
+    const queryString = queryParams.join("&");
+    this.props.history.push({
+      pathname: "/checkout",
+      search: "?" + queryString,
+    });
   };
 
   render() {
