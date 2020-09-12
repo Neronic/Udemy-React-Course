a62eadc81eba65ca514d193e6c54a22a8518ebb0
Jacob <n3r0n1c@hotmail.com>
2020-09-12 13:03:07 -0400

    chapter 16 complete

--- Chapter 16 - Redux Advanced/Course Work/redux-advanced/src/store/reducers/result.js
+++ Chapter 16 - Redux Advanced/Course Work/redux-advanced/src/store/reducers/result.js
@@ -1,27 +1,25 @@
-import * as actionTypes from "../actions/actions";
+import * as actionTypes from "../actions/actionsTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
   results: [],
 };
 
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(
+    (result) => result.id !== action.resultElId
+  );
+  return updateObject(state, { results: updatedArray });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat({ id: new Date(), value: action.result }),
-      };
+      });
     case actionTypes.DELETE_RESULT:
-      // const id = 2;
-      // const newArray = [...state.results];
-      // newArray.splice(id, 1)
-      const updatedArray = state.results.filter(
-        (result) => result.id !== action.resultElId
-      );
-      return {
-        ...state,
-        results: updatedArray,
-      };
+      return deleteResult(state, action);
   }
   return state;
 };
