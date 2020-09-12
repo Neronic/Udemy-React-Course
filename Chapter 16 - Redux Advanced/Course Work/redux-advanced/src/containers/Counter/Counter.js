8f5c507995240b27ad3233615d27abdd32dfb7a4
Jacob <n3r0n1c@hotmail.com>
2020-09-12 11:00:10 -0400

    Chapter 16 progress

--- Chapter 16 - Redux Advanced/Course Work/redux-advanced/src/containers/Counter/Counter.js
+++ Chapter 16 - Redux Advanced/Course Work/redux-advanced/src/containers/Counter/Counter.js
@@ -1,48 +1,62 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
-import CounterControl from '../../components/CounterControl/CounterControl';
-import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import * as actionTypes from '../../store/actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actionCreators from "../../store/actions/actions";
+import CounterControl from "../../components/CounterControl/CounterControl";
+import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
 class Counter extends Component {
-    render () {
-        return (
-            <div>
-                <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
-                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
-                <hr />
-                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
-                <ul>
-                    {this.props.storedResults.map(strResult => (
-                        <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
-                    ))}
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <CounterOutput value={this.props.ctr} />
+        <CounterControl
+          label="Increment"
+          clicked={this.props.onIncrementCounter}
+        />
+        <CounterControl
+          label="Decrement"
+          clicked={this.props.onDecrementCounter}
+        />
+        <CounterControl label="Add 10" clicked={this.props.onAddCounter} />
+        <CounterControl
+          label="Subtract 15"
+          clicked={this.props.onSubtractCounter}
+        />
+        <hr />
+        <button onClick={() => this.props.onStoreResult(this.props.ctr)}>
+          Store Result
+        </button>
+        <ul>
+          {this.props.storedResults.map((strResult) => (
+            <li
+              key={strResult.id}
+              onClick={() => this.props.onDeleteResult(strResult.id)}
+            >
+              {strResult.value}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-
-        ctr: state.ctr.counter,
-        storedResults: state.res.results
-    }
+const mapStateToProps = (state) => {
+  return {
+    ctr: state.ctr.counter,
+    storedResults: state.res.results,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
-        onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
-        onAddCounter: () => dispatch({type: actionTypes.ADD, val: 10}),
-        onSubtractCounter: () => dispatch({type: actionTypes.SUBTRACT, val: 15}),
-        onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result: result}),
-        onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElId: id})
-    }
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrementCounter: () => dispatch(actionCreators.increment()),
+    onDecrementCounter: () => dispatch(actionCreators.decrement()),
+    onAddCounter: () => dispatch(actionCreators.add(10)),
+    onSubtractCounter: () => dispatch(actionCreators.subtract(15)),
+    onStoreResult: (result) => dispatch(actionCreators.storeResult(result)),
+    onDeleteResult: (id) => dispatch(actionCreators.deleteResult(id)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
