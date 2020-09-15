a842a75b8e3fb86b3c686223beab251a3ecf7c10
Jacob <n3r0n1c@hotmail.com>
2020-09-14 21:40:45 -0400

    Chapter 18 progress

--- Chapter 18 - Adding Authentication to our Burger Project/Course Work/burger-builder/src/containers/Checkout/Checkout.js
+++ Chapter 18 - Adding Authentication to our Burger Project/Course Work/burger-builder/src/containers/Checkout/Checkout.js
@@ -3,7 +3,7 @@ import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSumm
 import { Route, Redirect } from "react-router-dom";
 import ContactData from "./ContactData/ContactData";
 import { connect } from "react-redux";
-import * as actions from "../../store/actions/index";
+//import * as actions from "../../store/actions/index";
 
 class Checkout extends Component {
   checkoutCancelledHandler = () => {
---
+++ Chapter 18 - Adding Authentication to our Burger Project/Course Work/burger-builder/src/store/actions/auth.js
@@ -0,0 +1,46 @@
+import * as actionTypes from "./actionTypes";
+import axios from "axios";
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
+
+export const authSuccess = (authData) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData: authData,
+  };
+};
+
+export const authFail = (error) => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error,
+  };
+};
+
+export const auth = (email, password) => {
+  return (dispatch) => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDMZRpO-hlgXTSdt9D-6P1H4P7-d17YInI",
+        authData
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
+  };
+};
