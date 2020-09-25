ef2e114a8cc61e9eec50a7c631eff82e2d6c60e6
Jacob <n3r0n1c@hotmail.com>
2020-09-25 17:46:09 -0400

    Chapter 27 progress

--- Chapter 28 - Bonus - Replacing Redux with React Hooks/Course Work/replace-redux/src/containers/Favorites.js
+++ Chapter 28 - Bonus - Replacing Redux with React Hooks/Course Work/replace-redux/src/containers/Favorites.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useContext } from "react";
+import { ProductsContext } from "../context/products-context";
+import FavoriteItem from "../components/Favorites/FavoriteItem";
+import "./Products.css";
 
-import FavoriteItem from '../components/Favorites/FavoriteItem';
-import './Products.css';
-
-const Favorites = props => {
-  const favoriteProducts = useSelector(state =>
-    state.shop.products.filter(p => p.isFavorite)
+const Favorites = (props) => {
+  const favoriteProducts = useContext(ProductsContext).products.filter(
+    (p) => p.isFavorite
   );
   let content = <p className="placeholder">Got no favorites yet!</p>;
   if (favoriteProducts.length > 0) {
     content = (
       <ul className="products-list">
-        {favoriteProducts.map(prod => (
+        {favoriteProducts.map((prod) => (
           <FavoriteItem
             key={prod.id}
             id={prod.id}
