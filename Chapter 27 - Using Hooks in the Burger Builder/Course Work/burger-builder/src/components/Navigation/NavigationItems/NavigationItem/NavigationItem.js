edcfb2771dff2aa8c858fe4de759016420d30b01
Jacob <n3r0n1c@hotmail.com>
2020-09-25 14:58:31 -0400

    Chapter 27 Set up

---
+++ Chapter 27 - Using Hooks in the Burger Builder/Course Work/burger-builder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import classes from "./NavigationItem.css";
+
+const navigationItem = (props) => (
+  <li className={classes.NavigationItem}>
+    <NavLink exact activeClassName={classes.active} to={props.link}>
+      {props.children}
+    </NavLink>
+  </li>
+);
+
+export default navigationItem;
